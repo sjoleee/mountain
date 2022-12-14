d4a45cb3646ea211d5b6f8b2ac540b8491f9d032
조 건형 <qwes3123@gmail.com>
2022-12-14 16:17:08 +0900

    initialize commit

---
+++ server/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SampleModule } from './sample/sample.module';
+
+@Module({
+  imports: [SampleModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
